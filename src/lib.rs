c6a2b02c996db0628c8b615860927fdba9168bf6
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-01-25 14:55:24 +0100

    Add remove_component

--- src/lib.rs
+++ src/lib.rs
@@ -70,5 +70,76 @@ pub struct Net<'a> {
     pub nodes: Vec<Node<'a>>,
 }
 
+impl<'a> NetList<'a> {
+    pub fn remove_component(&mut self, reference: &str) {
+        if let Some(index) = self
+            .components
+            .iter()
+            .position(|comp| comp.reference == reference)
+        {
+            let comp = &self.components[index];
+
+            for net in self.nets.iter_mut() {
+                net.nodes.retain(|node| node.reference != reference);
+            }
+
+            self.nets.retain(|net| !net.nodes.is_empty());
+
+            if self
+                .components
+                .iter()
+                .filter(|c| c.part_id == comp.part_id)
+                .count()
+                == 1
+            {
+                if let Some(part_index) = self
+                    .parts
+                    .iter()
+                    .position(|part| comp.part_id == part.part_id)
+                {
+                    self.parts.remove(part_index);
+                }
+            }
+
+            self.components.remove(index);
+        }
+    }
+}
+
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    macro_rules! test_data {
+        ($fname:expr) => {
+            std::fs::read_to_string(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/resources/test/",
+                $fname
+            ))
+            .unwrap()
+        };
+    }
+
+    #[test]
+    fn remove_component_works() {
+        let input = test_data!("kvt.net");
+        let mut netlist: NetList = (&input).try_into().unwrap();
+
+        assert_eq!(netlist.components.len(), 4);
+        assert_eq!(netlist.parts.len(), 3);
+        assert_eq!(netlist.nets.len(), 7);
+
+        netlist.remove_component("R1");
+
+        assert_eq!(netlist.components.len(), 3);
+        assert_eq!(netlist.parts.len(), 2);
+        assert_eq!(netlist.nets.len(), 7);
+
+        netlist.remove_component("U2");
+
+        assert_eq!(netlist.components.len(), 2);
+        assert_eq!(netlist.parts.len(), 2);
+        assert_eq!(netlist.nets.len(), 6);
+    }
+}
